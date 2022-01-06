264b78ce67d7b9aa384200ad9d1992e73e8fa9de
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-01-06 14:06:43 -0500

    Add fox and fox animation

--- testing/graph.py
+++ testing/graph.py
@@ -2,28 +2,38 @@ from manim import *
 
 class LinearGraph(Scene):
     def construct(self):
+        # Create 3 vertex path graph
         vertices = [1, 2, 3]
         edges = [(1, 2), (2, 3)]
         layout = {1: [-2, 0, 0], 2: [0, 0, 0], 3: [2, 0, 0]}
         g = Graph(vertices, edges, layout=layout)
 
+        # Initialize hare object
         hare = ImageMobject("assets/hare.png")
-        hare.scale(0.3)
+        hare.scale(0.24)
         hare.next_to(g.vertices[1], UP)
 
-        text = MarkupText(f"{g.vertices}", color=RED)
+        # Initialize fox object
+        fox = ImageMobject("assets/fox.png")
+        fox.scale(0.6)
+        fox.next_to(g.vertices[2], UP)
+
+        # Make text
+        text = MarkupText(f"The hare is deceased", color=RED)
         text.next_to(g, DOWN, buff=1)
 
+        # Create graph
         self.play(Create(g))
         self.wait(1)
 
+        # Add hare
         self.play(FadeIn(hare, shift=UP, scale=0.1))
+        self.play(FadeIn(fox, shift=UP, scale=0.1))
 
+        # Move hare to the other node
+        self.play(fox.animate(run_time=2).next_to(g.vertices[1], UP))
 
-        self.play(hare.animate(run_time=2).next_to(g.vertices[3], UP))
-
-
+        # Add text and pause
         self.play(Create(text))
-        
-
         self.wait(2)
+        
