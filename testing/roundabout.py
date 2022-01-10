fbb1e7290acf613e17d4638ee9d07d5c0dd0c587
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-01-10 10:52:23 -0500

    Improve cycle graph text
    Make text sizes consistent

--- testing/roundabout.py
+++ testing/roundabout.py
@@ -1,15 +1,18 @@
 from manim import *
 
+
 class CycleGraph(Scene):
     def construct(self):
 
-        timeOfNextText = 6
+        next_text_time = 4
+        animation_time = 1
 
         # Create 6 cycle graph
         vertices = [1, 2, 3, 4, 5, 6]
         edges = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 1)]
-        layout = {1: [-2, 1, 0], 2: [0, 2, 0], 3: [2, 1, 0], 4: [2, -1, 0], 5: [0, -2, 0], 6: [-2, -1, 0]}
-        g = Graph(vertices, edges, layout=layout) #, layout=layout)
+        layout = {1: [-2, 1, 0], 2: [0, 2, 0], 3: [2, 1, 0],
+                  4: [2, -1, 0], 5: [0, -2, 0], 6: [-2, -1, 0]}
+        g = Graph(vertices, edges, layout=layout)  # , layout=layout)
 
         # Initialize hare object
         hare = ImageMobject("assets/hare.png")
@@ -22,67 +25,75 @@ class CycleGraph(Scene):
         fox.move_to(g.vertices[6])
 
         # Make text
-        text1 = MarkupText(f"The hare and fox are", color=RED)
-        text2 = MarkupText(f"engaging in shennanigans.", color=RED)
-        text1.next_to(g.vertices[5], DOWN, buff=0.3)
-        text2.next_to(text1, DOWN, buff=0.2)
+        text1_1 = MarkupText(f"The hare and fox are", color=RED)
+        text1_2 = MarkupText(f"engaging in shennanigans.", color=RED)
+        text1_1.next_to(g.vertices[5], DOWN, buff=0.3)
+        text1_2.next_to(text1_1, DOWN, buff=0.2)
 
-        # make text that says "the distance between the hare and the fox is always (v/2) - 1"
-        text3 = MarkupText(f"The distance between the hare and the fox is always (v/2) - 1.", color=RED)
-        text3.scale(0.8)
-        text3.next_to(g.vertices[5], DOWN, buff=0.3)
+        text2_1 = MarkupText(f"The distance between the hare", color=RED)
+        text2_2 = MarkupText(f"and the fox is always (n/2) - 1.", color=RED)
+        text2_1.next_to(g.vertices[5], DOWN, buff=0.3)
+        text2_2.next_to(text2_1, DOWN, buff=0.2)
 
-        # where v is the number of vertices in the cycle graph
-        text4 = MarkupText(f"Where v is the number of vertices in the cycle graph.", color=RED)
-        text4.scale(0.7)
-        text4.next_to(text3, DOWN, buff=0.2)
+        text3_1 = MarkupText(f"where n is the number of", color=RED)
+        text3_2 = MarkupText(f"vertices in the cycle graph.", color=RED)
+        text3_1.next_to(g.vertices[5], DOWN, buff=0.3)
+        text3_2.next_to(text3_1, DOWN, buff=0.2)
 
-        # because V is greater than or equal to 4 and the distance is always (v/2) - 1
-        # the distance between the hare and the fox is always atleast 1 
-        text5 = MarkupText(f"Because V is greater than or equal to 4 and the distance is always (v/2) - 1", color=RED)
-        text5.scale(0.65)
-        text5.next_to(g.vertices[5], DOWN, buff=0.3)
-        
-        # the distance between the hare and the fox is always atleast 1
-        text6 = MarkupText(f"The distance between the hare and the fox is always atleast 1.", color=RED)
-        text6.scale(0.65)
-        text6.next_to(text5, DOWN, buff=0.2)
+        text4_1 = MarkupText(f"Because n is greater than or equal to 4,", color=RED)
+        text4_2 = MarkupText(f"and the distance is always (n/2) - 1,", color=RED)
+        text4_1.next_to(g.vertices[5], DOWN, buff=0.3)
+        text4_2.next_to(text4_1, DOWN, buff=0.2)
 
+        text5_1 = MarkupText(f"the distance between the hare ", color=RED)
+        text5_2 = MarkupText(f"and the fox is always at least 1.", color=RED)
+        text5_1.next_to(g.vertices[5], DOWN, buff=0.3)
+        text5_2.next_to(text5_1, DOWN, buff=0.2)
 
-
+        text6_1 = MarkupText(f"Thus, the hare is not deceased.", color=RED)
+        text6_1.next_to(g.vertices[5], DOWN, buff=0.3)
 
         # Create graph
         self.play(Create(g))
         self.wait(0.5)
 
-        # Add hare
+        # Add hare and fox
         self.play(FadeIn(hare, shift=UP, scale=0.1))
         self.play(FadeIn(fox, shift=UP, scale=0.1))
 
         # Add text and pause
-        self.play(Create(text1), Create(text2))
-        self.wait(1)
+        self.play(Create(text1_1), Create(text1_2))
+        self.wait(0.7)
 
         # Move animals
-        for i in range(15):
-            self.play(fox.animate(run_time=0.6).move_to(g.vertices[(i+6)%6 + 1]))
-            self.play(hare.animate(run_time=0.6).move_to(g.vertices[(i+3)%6 + 1]))
+        for i in range(25):
+            if i == next_text_time - 1:
+                self.play(FadeOut(text1_1), FadeOut(text1_2), hare.animate(run_time=animation_time).move_to(g.vertices[(i+3) % 6 + 1]), fox.animate(run_time=animation_time).move_to(g.vertices[(i+6) % 6 + 1]))
+            elif i == next_text_time:
+                self.play(Create(text2_1), Create(text2_2), hare.animate(run_time=animation_time).move_to(g.vertices[(i+3) % 6 + 1]), fox.animate(run_time=animation_time).move_to(g.vertices[(i+6) % 6 + 1]))
+            elif i == next_text_time + 3:
+                self.play(FadeOut(text2_1), FadeOut(text2_2), hare.animate(run_time=animation_time).move_to(g.vertices[(i+3) % 6 + 1]), fox.animate(run_time=animation_time).move_to(g.vertices[(i+6) % 6 + 1]))
+            elif i == next_text_time + 4:
+                self.play(Create(text3_1), Create(text3_2), hare.animate(run_time=animation_time).move_to(g.vertices[(i+3) % 6 + 1]), fox.animate(run_time=animation_time).move_to(g.vertices[(i+6) % 6 + 1]))
+            elif i == next_text_time + 7:
+                self.play(FadeOut(text3_1), FadeOut(text3_2), hare.animate(run_time=animation_time).move_to(g.vertices[(i+3) % 6 + 1]), fox.animate(run_time=animation_time).move_to(g.vertices[(i+6) % 6 + 1]))
+            elif i == next_text_time + 8:
+                self.play(Create(text4_1), Create(text4_2), hare.animate(run_time=animation_time).move_to(g.vertices[(i+3) % 6 + 1]), fox.animate(run_time=animation_time).move_to(g.vertices[(i+6) % 6 + 1]))
+            elif i == next_text_time + 11:
+                self.play(FadeOut(text4_1), FadeOut(text4_2), hare.animate(run_time=animation_time).move_to(g.vertices[(i+3) % 6 + 1]), fox.animate(run_time=animation_time).move_to(g.vertices[(i+6) % 6 + 1]))
+            elif i == next_text_time + 12:
+                self.play(Create(text5_1), Create(text5_2), hare.animate(run_time=animation_time).move_to(g.vertices[(i+3) % 6 + 1]), fox.animate(run_time=animation_time).move_to(g.vertices[(i+6) % 6 + 1]))
+            elif i == next_text_time + 15:
+                self.play(FadeOut(text5_1), FadeOut(text5_2), hare.animate(run_time=animation_time).move_to(g.vertices[(i+3) % 6 + 1]), fox.animate(run_time=animation_time).move_to(g.vertices[(i+6) % 6 + 1]))
+            elif i == next_text_time + 16:
+                self.play(Create(text6_1), hare.animate(run_time=animation_time).move_to(g.vertices[(i+3) % 6 + 1]), fox.animate(run_time=animation_time).move_to(g.vertices[(i+6) % 6 + 1]))
+            else:
+                self.play(hare.animate(run_time=animation_time).move_to(g.vertices[(i+3) % 6 + 1]), fox.animate(run_time=animation_time).move_to(g.vertices[(i+6) % 6 + 1]))
 
-            # fade out the text 
-            if i == timeOfNextText:
-                self.play(FadeOut(text1), FadeOut(text2))
-                self.play(Create(text3))
-            if i == timeOfNextText + 1:
-                self.play(Create(text4))
-
-            if i == timeOfNextText + 3:
-                self.play(FadeOut(text3), FadeOut(text4))
-                self.play(Create(text5))
-            if i == timeOfNextText + 4:
-                self.play(Create(text6))
-
-        self.play(FadeOut(text5), FadeOut(text6))
+        # Fade out text
+        self.play(FadeOut(text6_1))
         self.wait()
-        # Fade out everything 
+
+        # Fade out everything
         self.play(FadeOut(hare), FadeOut(fox), Uncreate(g))
         self.wait()
