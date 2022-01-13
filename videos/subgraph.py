12f90bec5b22a3eb217516af6d7840ce38f729d9
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-01-12 21:05:54 -0500

    Complete subgraph part

--- videos/subgraph.py
+++ videos/subgraph.py
@@ -23,9 +23,11 @@ class Subgraph(Scene):
         text1_2.next_to(text1_1, DOWN, buff=BETWEEN_TEXT_BUFFER)
 
         text2_1 = MarkupText(f"This 2-fox graph contains three", color=TEXT_COLOR)
-        text2_2 = MarkupText(f"sub-cycles: a 5-cycle, and two 3-cycles.", color=TEXT_COLOR)
+        text2_2 = MarkupText(f"sub-cycles: two 3-cycles,<span foreground=\"BLACK\"> and a 5-cycle.</span>", color=TEXT_COLOR)
+        text2_2_2 = MarkupText(f"sub-cycles: two 3-cycles, and a 5-cycle.", color=TEXT_COLOR)
         text2_1.shift(SubgraphConstants.TEXT_SHIFT)
         text2_2.next_to(text2_1, DOWN, buff=BETWEEN_TEXT_BUFFER)
+        text2_2_2.next_to(text2_1, DOWN, buff=BETWEEN_TEXT_BUFFER)
 
         text3_1 = MarkupText(f"The largest fox number sub-cycle", color=TEXT_COLOR)
         text3_2 = MarkupText(f"is the 5-cycle graph, at 2.", color=TEXT_COLOR)
@@ -37,21 +39,24 @@ class Subgraph(Scene):
         text4_1.shift(SubgraphConstants.TEXT_SHIFT)
         text4_2.next_to(text4_1, DOWN, buff=BETWEEN_TEXT_BUFFER)
 
+        text5_1 = MarkupText(f"The hare is not deceased", color=TEXT_COLOR).shift(DOWN * 0.25)
+        text5_1.shift(SubgraphConstants.TEXT_SHIFT)
+
         # Create graph objects
         self.play(Create(graph))
-        self.wait(2)
-
-        # Cycle through text, begin truncated times
-        self.play(Write(text1_1), Write(text1_2))
         self.wait(PAUSE_TIME)
+
+        # Cycle through text
+        self.play(Write(text1_1), Write(text1_2))
+        self.wait(LONG_PAUSE_TIME)
 
         self.play(ReplacementTransform(text1_1, text2_1),
             ReplacementTransform(text1_2, text2_2))
-        self.wait(PAUSE_TIME)
+        self.wait()
 
         # Indicate subgraphs sequentially
         self.play(*bulk_indicate(graph, [(1, 3), (2, 11), (2, 13)]),
-            run_time=ANIMATION_TIME, *bulk_indicate_points(graph, [11, 2, 13]))
+            *bulk_indicate_points(graph, [11, 2, 13]))
         self.wait(PAUSE_TIME / 2)
 
         self.play(*bulk_indicate(graph, [(16, 18), (18, 7), (7, 16)]),
@@ -59,37 +64,43 @@ class Subgraph(Scene):
         self.wait(PAUSE_TIME / 2)
 
         self.play(*bulk_indicate(graph, [(1, 3), (3, 4), (4, 5), (5, 6), 
-            (16, 18), (8, 1)]), 
+                    (16, 18), (8, 1)]), 
             *bulk_indicate_points(graph, [11, 13, 4, 5, 16, 18]))
 
-        self.wait(PAUSE_TIME)
-        self.play(graph.animate(run_time=ANIMATION_TIME * 2).change_layout(SubgraphConstants.EXPLODED_LAYOUT))
+        self.play(ReplacementTransform(text2_2, text2_2_2))
+        self.wait(LONG_PAUSE_TIME)
+
+        # Explode the graph and shift up slightly
+        self.play(graph.animate(run_time=ANIMATION_TIME * 2)
+                .change_layout(SubgraphConstants.EXPLODED_LAYOUT))
+        self.play(graph.animate(run_time=ANIMATION_TIME / 2).shift(UP * 0.7))
 
         # Continue iterating through text
         self.play(ReplacementTransform(text2_1, text3_1),
-            ReplacementTransform(text2_2, text3_2))
-        self.wait(PAUSE_TIME)
+            ReplacementTransform(text2_2_2, text3_2))
+        self.wait(LONG_PAUSE_TIME)
 
         self.play(ReplacementTransform(text3_1, text4_1),
             ReplacementTransform(text3_2, text4_2))
-        self.wait(PAUSE_TIME)
+        self.wait(LONG_PAUSE_TIME)
 
-        # Reunite graph, show fox and hare        # End time truncation section
-        self.play(graph.animate(run_time=ANIMATION_TIME * 2).change_layout(SubgraphConstants.FULL_LAYOUT))
-        self.play(Create(hare), Create(fox))
-        self.wait(PAUSE_TIME)
-
-        # Indicate starting positions
-        self.play(*bulk_indicate_points(graph, [1]))
+        # Reunite graph, show fox and hare
+        self.play(graph.animate(run_time=ANIMATION_TIME * 2)
+                .change_layout(SubgraphConstants.FULL_LAYOUT))
+        self.play(Create(hare), Create(fox), Uncreate(text4_1), Uncreate(text4_2))
         self.wait(PAUSE_TIME)
 
         # Play game
         for hare_pos, fox_pos in SubgraphConstants.MOVES:
-            self.play(fox.animate(run_time=ANIMATION_TIME).move_to(graph.vertices[fox_pos]))
+            self.play(fox.animate(run_time=ANIMATION_TIME / 2).move_to(graph.vertices[fox_pos]))
             self.wait(0.1)
-            self.play(hare.animate(run_time=ANIMATION_TIME).move_to(graph.vertices[hare_pos]))
+            self.play(hare.animate(run_time=ANIMATION_TIME / 2).move_to(graph.vertices[hare_pos]))
             self.wait(PAUSE_TIME / 2)
 
+        # Show final text
+        self.play(Create(text5_1))
+        self.wait(LONG_PAUSE_TIME)
 
+        # Destroy objects and graph
+        self.play(Uncreate(hare), Uncreate(fox), Uncreate(graph), Uncreate(text5_1))
         self.wait(PAUSE_TIME)
-
