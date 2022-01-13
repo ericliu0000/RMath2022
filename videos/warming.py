70cbe3d64cc7de8ed6990bd9c6e52d07c74d1191
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-01-12 21:55:05 -0500

    Implement additional funny

--- videos/warming.py
+++ videos/warming.py
@@ -3,7 +3,7 @@ from constants import *
 
 class Funny(Scene):
     def construct(self):
-        g1 = Graph([1, 2, 3, 4, 5], [(1, 2), (2, 3), (3, 4), (4, 5), (5, 1)], layout="kamada_kawai")
+        g1 = Graph(list(range(30)), [(1, 2), (2, 3), (3, 4), (4, 5), (5, 1)], layout="kamada_kawai")
         g1.shift(LEFT * 2)
 
         g2 = Graph([1, 2], [(1, 2)])
@@ -25,13 +25,14 @@ class Funny(Scene):
         self.wait(LONG_PAUSE_TIME)
 
         text = MarkupText(f"global warming", color=TEXT_COLOR)
-        self.play(Write(text, run_time=8), fox.animate(run_time=8).shift(RIGHT * 2.3), hare.animate(run_time=8).shift(UP * 0.9))
-        self.play(g1.animate.change_layout(layout="spring"), g2.animate.change_layout(layout="spring"))
-
         text2 = MarkupText(f"they are both deceased", color=TEXT_COLOR)
+        text3 = MarkupText(f"hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh", color=GREEN).shift(UP)
+
+        self.play(Write(text, run_time=8), fox.animate(run_time=8).shift(RIGHT * 2.3), hare.animate(run_time=8).shift(UP * 0.9))
+        self.play(g1.animate.change_layout(layout="random"), g2.animate.change_layout(layout="spring"))
+
         self.play(Write(text2, run_time=4), fox.animate(run_time=4).shift(UP * 7), hare.animate(run_time=4).shift(DOWN * 7))
 
         self.wait(PAUSE_TIME)
-        self.play(text2.animate.shift(DOWN + RIGHT))
-        self.wait(LONG_PAUSE_TIME)
+        self.play(text2.animate.shift(DOWN + RIGHT), Write(text3, run_time=8))
         
